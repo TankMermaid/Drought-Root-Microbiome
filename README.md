7518e5266bd789223751d4ae5226b6e4fe6bc672
TankMermaid <xnzhang@genetics.ac.cn>
2020-11-24 20:19:52 +0800

    submit

--- README.md
+++
@@ -1,2 +0,0 @@
-# Drought-Root-Microbiome
-This repository contains all the data and code used for the paper
---
+++ temp/demo.py
@@ -0,0 +1,105 @@
+#########################################################################
+# File Name: demo.py
+# Author: Xiao-Ning Tank Zhang
+# mail: xnzhang@genetics.ac.cn
+# Created Time: Tue 24 Nov 2020 08:08:30 PM CST
+#########################################################################
+#!/bin/python
+
+## this is a demo programm displaying some feature of vim  and zsh function
+
+JFIAJIFAIJ
+
+
+
+FJAIJFIAJIF
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+FAJIJFIAJIFJAIFJIA
+
+
+
+FKAIOJFIAJIFJAI
+JFIAJFIAJ
+
+
+
+FJAIJFIAJIFJAI
+
+
+
+FJAIFJAIJFIA
+JIFAJIJFAIF
+# File Name: demo.py # Author: Xiao-Ning Tank Zhang
+# mail: xnzhang@genetics.ac.cn
+# Created Time: Tue 24 Nov 2020 08:08:30 PM CST
+#########################################################################
+#!/bin/python
+
+## this is a demo programm displaying some feature of vim  and zsh function
+
+
+
